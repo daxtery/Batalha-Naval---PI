7980433a70dd0312d68d22d74f3c0a8fba04e5b0
David Pinto <daxtery11@gmail.com>
2020-09-06 21:47:48 +0100

    Remove unused Horizontal, Vertical directions

--- src/Common/Direction.java
+++ src/Common/Direction.java
@@ -8,20 +8,12 @@ public enum Direction {
     Right,
     Up,
     Down;
-//    HORIZONTAL,
-//    VERTICAL;
 
     public Direction rotated;
     public Point vector;
     public Direction opposite;
 
     static {
-//        HORIZONTAL.rotated = VERTICAL;
-//        HORIZONTAL.vector = new Point(0,1);
-//
-//        VERTICAL.rotated = HORIZONTAL;
-//        VERTICAL.vector = new Point(1,0);
-
         Left.vector = new Point(0,-1);
         Left.rotated = Up;
         Left.opposite = Right;
@@ -35,16 +27,12 @@ public enum Direction {
         Down.opposite = Up;
 
         Up.vector = new Point(-1,0);
-        Up.rotated = Left;
+        Up.rotated = Right;
         Up.opposite = Down;
     }
 
     public Direction getOpposite(){
         return opposite;
-    }
-
-    public Direction getRotated(){
-        return rotated;
     }
 
 }
